4d65cef1408a2b5872871f2164cd5dc0ea1849b1
Falkenhorst14 <77630523+Falkenhorst14@users.noreply.github.com>
2024-10-16 22:54:59 +0200

    Populated Spinner mit Test-Daten

--- app/src/main/java/com/example/mentorlink/DetailAbschlussarbeit.java
+++ app/src/main/java/com/example/mentorlink/DetailAbschlussarbeit.java
@@ -1,6 +1,8 @@
 package com.example.mentorlink;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DetailAbschlussarbeit extends AppCompatActivity {
+
+
+    private Spinner spnDetailAbschlussarbeitKategorie;
+    private Spinner spnDetailAbschlussarbeitStatus;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,23 @@ public class DetailAbschlussarbeit extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spnDetailAbschlussarbeitKategorie = findViewById(R.id.spnDetailAbschlussarbeitKategorie);
+        spnDetailAbschlussarbeitStatus = findViewById(R.id.spnDetailAbschlussarbeitStatus);
+
+        String[] arrKat = new String[] {"Wirtschaft & Soziales"};
+        String[] arrStat = new String[] {"in Bearbeitung"};
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrKat);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnDetailAbschlussarbeitKategorie.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrStat);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnDetailAbschlussarbeitStatus.setAdapter(adapter2);
+
+
     }
 }
